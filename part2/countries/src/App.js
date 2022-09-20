28afc0d11616334be603ec68f2f783ee37d4143c
Aparajitha Murali <aparajitha.m7@gmail.com>
2022-09-20 12:55:45 +0530

    until 2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,57 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const Languages = ({language}) =>
+{
+  console.log(language)
+  return(
+    <div>
+ <li>{language}</li>
+    </div>
+  )
+}
+const SingleCountry = ({country})=>{
+  console.log(country[0]);
+  const selectedCountry = country[0]
+  return(
+    <div>
+     <h2>{selectedCountry.name.common}</h2>
+     <p>capital {selectedCountry.capital[0]}</p>
+     <p>area {selectedCountry.area}</p>
+     <h3>Languages:</h3>
+     <ul>
+      {Object.entries(selectedCountry.languages).map(([code,language]) => 
+       
+        <Languages key={code} language={language}/>
+              
+      )}
+     </ul>
+    </div>
+  )
+}
+
+const CountryDisplay = ({filteredCountries}) =>
+
+{console.log(filteredCountries);
+  if (filteredCountries.length >10) {
+    return (
+      <div>
+        <p>Too many matches. Please specify another filter</p>
+      </div>
+    )
+  }
+  else if (filteredCountries.length <= 10 && filteredCountries.length >1){
+    return <Countries countries={filteredCountries}/>
+  }
+  else if (filteredCountries.length === 1) {
+    return <SingleCountry country={filteredCountries}/>
+    }
+  else{
+    return <p></p>
+  }
+}
+
+
 const CountryName = ({country}) => {
 
   return(
@@ -10,7 +61,7 @@ const CountryName = ({country}) => {
   )
 }
 const Countries = ({countries}) => {
-
+console.log(countries)
   //using the cca3 as id because it is unique
   return (
     <div>
@@ -40,11 +91,16 @@ const App = () => {
 
 
   const handleCountryChange = (event) => {
-    //console.log(event.target.value)
-    if (typeof event.target.value === 'string'){
+    console.log(event.target.value)
+    //if there is a string entered, start searching
+    if (typeof event.target.value === 'string' && event.target.value.length >0){
 const filtered = countries.filter(country =>country.name.common.toLowerCase().includes(event.target.value))
 console.log((filtered.length))
 setFilteredCountries(filtered)
+    }
+    else{
+      //if the search bar is empty or everthing in it is deleted by user
+      setFilteredCountries('')
     }
     setLookingForCountry(event.target.value)
   }
@@ -57,6 +113,7 @@ setFilteredCountries(filtered)
 */
   return (
     <div>
+      find countries
       <form>
         <input
           value={lookingForCountry}
@@ -64,7 +121,7 @@ setFilteredCountries(filtered)
         />
         
       </form>
-      <Countries countries={filteredCountries}/>
+      <CountryDisplay filteredCountries={filteredCountries}/>
     </div>
   )
 }
